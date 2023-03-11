99d187f52f62f2e77724abac06b5ad9711f6e273
aidana20 <127585155+aidana20@users.noreply.github.com>
2023-03-11 16:42:04 +0300

    сделала дз2

---
+++ DZ_2  Aidana 24-01/main.js
@@ -0,0 +1,38 @@
+// //Задание 1
+var arr= [10,20,30,50,235,3000,415,650, 555 ]
+    for (var i = 0; i < arr.length; i++){
+    var numbersSort = arr[i].toString()
+    if (numbersSort[0] === '1' || numbersSort[0] ==='2' || numbersSort[0]==='5') {
+        console.log(numbersSort)
+    }
+    else{
+        console.log('ERROR')
+    }
+}
+//Задание 2
+ for (var i = 20; i >= 0; i--) {
+    console.log(i);
+ }
+
+//ЗАДАНИЕ 3
+var  svetofor =prompt('Введите цвета светофора: красный, желтый или зеленый') .toLocaleLowerCase().trim()
+var object= {
+    name:'красный',
+    name1:'желтый',
+    name2:'зеленый'
+}
+switch (svetofor) {
+    case object.name:
+        alert( 'Стоп!');
+        break;
+    case object.name1:
+        alert( 'Приготовься!' );
+        break;
+    case object.name2:
+        alert( 'Иди!' );
+        break;
+    default:
+        alert( "Введите цвета!" );
+}
+
+
