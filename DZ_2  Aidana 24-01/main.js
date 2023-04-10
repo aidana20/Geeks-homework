94e68b50dd4ced2f49554328576cec629494dddf
aidana20 <127585155+aidana20@users.noreply.github.com>
2023-04-10 14:53:33 +0300

    Done DZ-2

--- DZ_2  Aidana 24-01/main.js
+++ DZ_2  Aidana 24-01/main.js
@@ -1,4 +1,4 @@
-// //Задание 1
+ //Задание 1
 var arr= [10,20,30,50,235,3000,415,650, 555 ]
     for (var i = 0; i < arr.length; i++){
     var numbersSort = arr[i].toString()
@@ -15,24 +15,30 @@ var arr= [10,20,30,50,235,3000,415,650, 555 ]
  }
 
 //ЗАДАНИЕ 3
-var  svetofor =prompt('Введите цвета светофора: красный, желтый или зеленый') .toLocaleLowerCase().trim()
+var  svetofor =prompt('Введите цвета светофора: красный, желтый или зеленый').toLocaleLowerCase().trim()
 var object= {
     name:'красный',
     name1:'желтый',
     name2:'зеленый'
 }
-switch (svetofor) {
-    case object.name:
-        alert( 'Стоп!');
-        break;
-    case object.name1:
-        alert( 'Приготовься!' );
-        break;
-    case object.name2:
-        alert( 'Иди!' );
-        break;
-    default:
-        alert( "Введите цвета!" );
-}
+ switch (svetofor) {
+     case object.name:
+         alert( 'Стоп!');
+         break;
+     case object.name1:
+         alert( 'Приготовься!' );
+         break;
+     case object.name2:
+         alert( 'Иди!' );
+         break;
+     default:
+         alert( "Введите цвета!" );
+ }
 
+ // Задание № 4
+ var arr = []
+ for (var i = 0; i < 11; i++) {
+     arr[i] = i
+ }
+ console.log(arr)
 
